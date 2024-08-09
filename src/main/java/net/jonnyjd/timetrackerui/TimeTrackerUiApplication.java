5b479eb873d7959640a47328832676ea1b1e2ed3
Johannes Dewender <github@JonnyJD.net>
2024-08-10 00:48:06 +0200

    start.spring.io with web and thymeleaf

---
+++ src/main/java/net/jonnyjd/timetrackerui/TimeTrackerUiApplication.java
@@ -0,0 +1,13 @@
+package net.jonnyjd.timetrackerui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackerUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTrackerUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/jonnyjd/timetrackerui/TimeTrackerUiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jonnyjd.timetrackerui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackerUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
